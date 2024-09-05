08368fd9549f1dee470c5b15de21b32b05eb3e05
Sashka <sasha.4123@mail.ru>
2024-09-04 21:53:47 -0400

    My suggestion!!!

---
+++ Hi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Are You happy?");
+            string answer = Console.ReadLine();
+
+            switch (answer.ToLower())
+            {
+                case "yes": Console.WriteLine("So am I!!!"); break;
+                case "no":
+                    Console.WriteLine("Why? You are so cute. You can't be unhappy.."); break;
+                default: Console.WriteLine($"I don't understand your answer(( Try again))"); break;
+            }
+        }
+    }
+}
+
